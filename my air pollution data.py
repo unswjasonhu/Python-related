0b12ad1ff3a242be6a4c1bb3b5a3a2b906f05f0a
Jason <jasonhu.au2>
2015-10-13 12:41:52 +1100

    Python matplotlib.pyplot sample

--- my air pollution data.py
+++ my air pollution data.py
@@ -64,4 +64,4 @@ count_kehu2  = count(save_sheet('Kehu2'))
 total_dosage = (sum_count(count_vijay1),sum_count(count_han1),sum_count(count_jason1),sum_count(count_xiaohong1),sum_count(count_kehu1))
 print total_dosage
 #figure_tatal = plt.subplot().bar(['Driving person1','D2','B','W1','W2'],total_dosage)
-#plt.show()
+plt.show()
